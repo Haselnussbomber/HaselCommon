cbbdb543dc8e697449ed983e52ee287a7d220721
Haselnussbomber <mail@haselnussbomber.de>
2023-08-27 13:45:32 +0200

    just move IconButton into ImGuiUtils

--- HaselCommon/Utils/ImGuiUtils.cs
+++ HaselCommon/Utils/ImGuiUtils.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Numerics;
 using System.Threading.Tasks;
 using Dalamud.Interface;
@@ -137,5 +138,42 @@ public static class ImGuiUtils
     {
         using (ImRaii.PushFont(UiBuilder.IconFont))
             ImGui.TextDisabled(icon.ToIconString());
+    }
+
+    public static Vector2 GetIconButtonSize(FontAwesomeIcon icon)
+    {
+        using var iconFont = ImRaii.PushFont(UiBuilder.IconFont);
+        return ImGui.CalcTextSize(icon.ToIconString()) + ImGui.GetStyle().FramePadding * 2;
+    }
+
+    public static bool IconButton(string key, FontAwesomeIcon icon, string tooltip, Vector2 size = default, bool disabled = false, bool active = false)
+    {
+        using var iconFont = ImRaii.PushFont(UiBuilder.IconFont);
+        if (!key.StartsWith("##")) key = "##" + key;
+
+        var disposables = new List<IDisposable>();
+
+        if (disabled)
+        {
+            disposables.Add(ImRaii.PushColor(ImGuiCol.Text, ImGui.GetStyle().Colors[(int)ImGuiCol.TextDisabled]));
+            disposables.Add(ImRaii.PushColor(ImGuiCol.ButtonActive, ImGui.GetStyle().Colors[(int)ImGuiCol.Button]));
+            disposables.Add(ImRaii.PushColor(ImGuiCol.ButtonHovered, ImGui.GetStyle().Colors[(int)ImGuiCol.Button]));
+        }
+        else if (active)
+        {
+            disposables.Add(ImRaii.PushColor(ImGuiCol.Button, ImGui.GetStyle().Colors[(int)ImGuiCol.ButtonActive]));
+        }
+
+        var pressed = ImGui.Button(icon.ToIconString() + key, size);
+
+        foreach (var disposable in disposables)
+            disposable.Dispose();
+
+        iconFont?.Dispose();
+
+        if (ImGui.IsItemHovered())
+            ImGui.SetTooltip(tooltip);
+
+        return pressed;
     }
 }
