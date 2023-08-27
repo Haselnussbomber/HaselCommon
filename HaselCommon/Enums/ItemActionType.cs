595faa03fddec35d8de1eb69345f9225f0fd35d2
Haselnussbomber <mail@haselnussbomber.de>
2023-08-27 17:38:41 +0200

    add IsUnlockable and HasAcquired

---
+++ HaselCommon/Enums/ItemActionType.cs
@@ -0,0 +1,16 @@
+namespace HaselCommon.Enums;
+
+public enum ItemActionType : ushort
+{
+    Minions = 853,
+    Bardings = 1013,
+    Mounts = 1322,
+    CrafterBooks = 2136,
+    Miscellaneous = 2633, // riding maps, blu totems, emotes/dances, hairstyles
+    Cards = 3357,
+    GathererBooks = 4107,
+    OrchestrionRolls = 25183,
+    FramersKits = 29459,
+    // FieldNotes = 19743, // bozjan field notes (server side)
+    FashionAccessories = 20086,
+}
--- HaselCommon/Extensions/Sheets/ItemExtensions.cs
+++ HaselCommon/Extensions/Sheets/ItemExtensions.cs
@@ -1,4 +1,9 @@
+using System.Linq;
+using FFXIVClientStructs.FFXIV.Client.Game.UI;
 using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using HaselCommon.Enums;
+using HaselCommon.Structs.Internal;
+using HaselCommon.Utils;
 using Lumina.Excel.GeneratedSheets;
 
 namespace HaselCommon.Extensions.Sheets;
@@ -21,4 +26,40 @@ public static class ItemExtensions
     {
         return item.RowId != 0 && !item.IsUntradable && !item.IsCollectable && IsAddonOpen(AgentId.ItemSearch);
     }
+
+    public static bool IsUnlockable(this Item item)
+    {
+        return item.ItemAction.Row != 0 && Enum.GetValues<ItemActionType>().Any(type => (ushort)type == item.ItemAction.Value!.Type);
+    }
+
+    public static unsafe bool HasAcquired(this Item item)
+    {
+        var action = item.ItemAction.Value;
+        if (action == null)
+        {
+            return false;
+        }
+
+        var type = (ItemActionType)action.Type;
+        if (type == ItemActionType.Cards)
+        {
+            var cardId = item.AdditionalData;
+            var card = Service.DataManager.GetExcelSheet<TripleTriadCard>()!.GetRow(cardId);
+            return card != null && UIState.Instance()->IsTripleTriadCardUnlocked((ushort)card.RowId);
+        }
+
+        using var data = new DisposableStruct<ItemActionUnlockedData>();
+        data.Ptr->ItemActionId = action.RowId;
+
+        if (type == ItemActionType.OrchestrionRolls || type == ItemActionType.FramersKits)
+        {
+            data.Ptr->ItemActionAdditionalData = item.AdditionalData;
+        }
+
+        return HasItemActionUnlocked.Value.Invoke(data);
+    }
+
+    internal unsafe delegate bool HasItemActionUnlockedDelegate(ItemActionUnlockedData* data, nint a2 = 0, nint a3 = 0);
+    internal static readonly Lazy<HasItemActionUnlockedDelegate> HasItemActionUnlocked
+        = new(() => MemoryUtils.GetDelegateForSignature<HasItemActionUnlockedDelegate>("E8 ?? ?? ?? ?? 84 C0 75 A6 32 C0"));
 }
