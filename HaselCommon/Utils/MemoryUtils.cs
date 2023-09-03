85067197777def9696a440c1fda58b5375a56918
Haselnussbomber <mail@haselnussbomber.de>
2023-09-03 21:22:14 +0200

    address cache is not needed

--- HaselCommon/Utils/MemoryUtils.cs
+++ HaselCommon/Utils/MemoryUtils.cs
@@ -1,20 +1,12 @@
-using System.Collections.Generic;
 using Dalamud.Memory;
 
 namespace HaselCommon.Utils;
 
 public static unsafe class MemoryUtils
 {
-    private static readonly Dictionary<string, nint> AddressCache = new();
-
     public static T GetDelegateForSignature<T>(string signature) where T : Delegate
     {
-        if (!AddressCache.TryGetValue(signature, out var address))
-        {
-            address = Service.SigScanner.ScanText(signature);
-            AddressCache.Add(signature, address);
-        }
-
+        var address = Service.SigScanner.ScanText(signature);
         return Marshal.GetDelegateForFunctionPointer<T>(address);
     }
 
