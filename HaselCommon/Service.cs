dffca7fa1d78d3aee8089b841be5508a057749f7
Haselnussbomber <mail@haselnussbomber.de>
2023-09-20 03:15:43 +0200

    add ExtendedLevel

--- HaselCommon/Service.cs
+++ HaselCommon/Service.cs
@@ -8,11 +8,12 @@ namespace HaselCommon;
 internal class Service
 {
     [PluginService] public static DalamudPluginInterface PluginInterface { get; private set; } = null!;
-    [PluginService] public static IClientState ClientState { get; private set; } = null!;
     [PluginService] public static Framework Framework { get; private set; } = null!;
-    [PluginService] public static IGameConfig GameConfig { get; private set; } = null!;
-    [PluginService] public static IGamepadState GamepadState { get; private set; } = null!;
+    [PluginService] public static IClientState ClientState { get; private set; } = null!;
     [PluginService] public static IDataManager DataManager { get; private set; } = null!;
+    [PluginService] public static IGameConfig GameConfig { get; private set; } = null!;
+    [PluginService] public static IGameGui GameGui { get; private set; } = null!;
+    [PluginService] public static IGamepadState GamepadState { get; private set; } = null!;
     [PluginService] public static ISigScanner SigScanner { get; private set; } = null!;
     [PluginService] public static ITextureProvider TextureProvider { get; private set; } = null!;
 }
---
+++ HaselCommon/Sheets/ExtendedLevel.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using System.Numerics;
+using Dalamud.Game.Text.SeStringHandling.Payloads;
+using Lumina.Excel.GeneratedSheets;
+
+namespace HaselCommon.Sheets;
+
+public class ExtendedLevel : Level
+{
+    private static readonly Dictionary<uint, ExtendedLevel?> ObjectIdCache = new();
+
+    public static ExtendedLevel? GetByObjectId(uint objId)
+    {
+        if (!ObjectIdCache.TryGetValue(objId, out var value))
+        {
+            value = FindRow<ExtendedLevel>(row => row.Object == objId);
+            ObjectIdCache.Add(objId, value);
+        }
+
+        return value;
+    }
+
+    public Vector2 GetCoords()
+    {
+        var map = Map.Value;
+        var c = map!.SizeFactor / 100.0f;
+        var x = 41.0f / c * (((X + map.OffsetX) * c + 1024.0f) / 2048.0f) + 1f;
+        var y = 41.0f / c * (((Z + map.OffsetY) * c + 1024.0f) / 2048.0f) + 1f;
+        return new(x, y);
+    }
+
+    public void OpenMap()
+    {
+        var map = Map.Value;
+        if (map == null)
+            return;
+
+        var terr = map.TerritoryType.Value;
+        if (terr == null)
+            return;
+
+        Service.GameGui.OpenMapWithMapLink(new MapLinkPayload(
+            terr.RowId,
+            map.RowId,
+            (int)(X * 1_000f),
+            (int)(Z * 1_000f)
+        ));
+    }
+
+    public float GetDistanceFromPlayer()
+    {
+        var localPlayer = Service.ClientState.LocalPlayer;
+        if (localPlayer == null || Territory.Row != Service.ClientState.TerritoryType)
+            return float.MaxValue; // far, far away
+
+        return Vector2.Distance(
+            new Vector2(localPlayer.Position.X, localPlayer.Position.Z),
+            new Vector2(X, Z)
+        );
+    }
+}
