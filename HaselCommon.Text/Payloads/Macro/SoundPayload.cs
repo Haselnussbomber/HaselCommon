1ed8c3145ade71b9e5379a8e392ab8ca535efe87
Haselnussbomber <mail@haselnussbomber.de>
2024-02-19 02:24:26 +0100

    identified Arg1 on SoundPayload as IsJingle

--- HaselCommon.Text/Payloads/Macro/SoundPayload.cs
+++ HaselCommon.Text/Payloads/Macro/SoundPayload.cs
@@ -3,6 +3,6 @@ namespace HaselCommon.Text.Payloads.Macro;
 [SeStringPayload(MacroCodes.Sound)] // n n
 public class SoundPayload : HaselMacroPayload
 {
-    public ExpressionWrapper? Arg1 { get; set; }
+    public ExpressionWrapper? IsJingle { get; set; }
     public ExpressionWrapper? SoundId { get; set; }
 }
