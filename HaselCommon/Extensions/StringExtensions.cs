13433f13ff1686acf14e84d19a16ad4314297c8a
Haselnussbomber <mail@haselnussbomber.de>
2024-03-02 01:15:55 +0100

    add StringExtensions.FirstCharToLower

--- HaselCommon/Extensions/StringExtensions.cs
+++ HaselCommon/Extensions/StringExtensions.cs
@@ -5,4 +5,7 @@ public static class StringExtensions
     //! https://stackoverflow.com/a/4405876
     public static string FirstCharToUpper(this string input)
         => string.IsNullOrEmpty(input) ? string.Empty : string.Concat(input[0].ToString().ToUpper(), input.AsSpan(1));
+
+    public static string FirstCharToLower(this string input)
+        => string.IsNullOrEmpty(input) ? string.Empty : string.Concat(input[0].ToString().ToLower(), input.AsSpan(1));
 }
