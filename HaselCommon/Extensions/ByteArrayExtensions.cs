9ed94d2bb504e24a4cededd70730a83ba56924d2
Haselnussbomber <mail@haselnussbomber.de>
2024-03-02 01:14:33 +0100

    drop System.IO.Hashing

--- HaselCommon/Extensions/ByteArrayExtensions.cs
+++
@@ -1,9 +0,0 @@
-using System.IO.Hashing;
-
-namespace HaselCommon.Extensions;
-
-public static class ByteArrayExtensions
-{
-    public static string GetHash(this byte[] input)
-        => BitConverter.ToInt64(XxHash3.Hash(input)).ToString("x");
-}
--- HaselCommon/Extensions/StringExtensions.cs
+++ HaselCommon/Extensions/StringExtensions.cs
@@ -1,5 +1,3 @@
-using System.Text;
-
 namespace HaselCommon.Extensions;
 
 public static class StringExtensions
@@ -7,7 +5,4 @@ public static class StringExtensions
     //! https://stackoverflow.com/a/4405876
     public static string FirstCharToUpper(this string input)
         => string.IsNullOrEmpty(input) ? string.Empty : string.Concat(input[0].ToString().ToUpper(), input.AsSpan(1));
-
-    public static string GetHash(this string input)
-        => Encoding.UTF8.GetBytes(input).GetHash();
 }
