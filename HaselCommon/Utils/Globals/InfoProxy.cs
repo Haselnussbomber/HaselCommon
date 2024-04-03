2f819fe0a225756a74819ca3ec33237cb5d60003
Haselnussbomber <mail@haselnussbomber.de>
2024-04-04 00:44:59 +0200

    add global InfoProxy getter

---
+++ HaselCommon/Utils/Globals/InfoProxy.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Reflection;
+using FFXIVClientStructs.Attributes;
+using FFXIVClientStructs.FFXIV.Client.UI.Info;
+
+namespace HaselCommon.Utils.Globals;
+
+public static unsafe class InfoProxy
+{
+    private static readonly Dictionary<Type, InfoProxyId> InfoProxyIdCache = [];
+
+    public static T* GetInfoProxy<T>(InfoProxyId id) where T : unmanaged
+        => (T*)InfoModule.Instance()->GetInfoProxyById((uint)id);
+
+    public static T* GetInfoProxy<T>() where T : unmanaged
+    {
+        var type = typeof(T);
+
+        if (!InfoProxyIdCache.TryGetValue(type, out var id))
+        {
+            var attr = type.GetCustomAttribute<InfoProxyAttribute>(false)
+                ?? throw new Exception($"Agent {type.FullName} is missing InfoProxyAttribute");
+
+            InfoProxyIdCache.Add(type, id = attr.ID);
+        }
+
+        return GetInfoProxy<T>(id);
+    }
+}
