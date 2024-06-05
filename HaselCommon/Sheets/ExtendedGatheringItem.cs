2668e7f6c0d84eb089cc1e9e8ce5a16e3b50bf30
Haselnussbomber <mail@haselnussbomber.de>
2024-06-06 00:49:14 +0200

    get hidden gathering points from game data

--- HaselCommon/Sheets/ExtendedGatheringItem.cs
+++ HaselCommon/Sheets/ExtendedGatheringItem.cs
@@ -27,38 +27,25 @@ public class ExtendedGatheringItem : GatheringItem
             if (_gatheringPoints != null)
                 return _gatheringPoints;
 
-            var GatheringPointSheet = GetSheet<ExtendedGatheringPoint>();
+            var pointBases = new HashSet<uint>();
 
-            //! https://github.com/Ottermandias/GatherBuddy/blob/56da5c9/GatherBuddy.GameData/Data/HiddenItems.cs
-            // TODO: update to use game data https://github.com/Ottermandias/GatherBuddy/commit/23be42e9ecabe2dd658b569bf6dda3112f58cce5
-            var pointBases = Item switch
+            if (IsHidden)
             {
-                7758 => [203],  // Grade 1 La Noscean Topsoil
-                7761 => [200],  // Grade 1 Shroud Topsoil
-                7764 => [201],  // Grade 1 Thanalan Topsoil
-                7759 => [150],  // Grade 2 La Noscean Topsoil
-                7762 => [209],  // Grade 2 Shroud Topsoil
-                7765 => [151],  // Grade 2 Thanalan Topsoil
-                10092 => [210], // Black Limestone
-                10094 => [177], // Little Worm
-                10097 => [133], // Yafaemi Wildgrass
-                12893 => [295], // Dark Chestnut
-                15865 => [30],  // Firelight Seeds
-                15866 => [39],  // Icelight Seeds
-                15867 => [21],  // Windlight Seeds
-                15868 => [31],  // Earthlight Seeds
-                15869 => [25],  // Levinlight Seeds
-                15870 => [14],  // Waterlight Seeds
-                12534 => [285], // Mythrite Ore
-                12535 => [353], // Hardsilver Ore
-                12537 => [286], // Titanium Ore
-                12579 => [356], // Birch Log
-                12878 => [297], // Cyclops Onion
-                12879 => [298], // Emerald Beans
-                _ => new HashSet<uint>()
-            };
+                var gatheringItemPointSheet = GetSheet<GatheringItemPoint>();
 
-            foreach (var point in GatheringPointSheet)
+                foreach (var row in gatheringItemPointSheet)
+                {
+                    if (row.RowId < RowId) continue;
+                    if (row.RowId > RowId) break;
+                    if (row.GatheringPoint.Row == 0) continue;
+
+                    pointBases.Add(row.GatheringPoint.Value!.GatheringPointBase.Row);
+                }
+            }
+
+            var gatheringPointSheet = GetSheet<ExtendedGatheringPoint>();
+
+            foreach (var point in gatheringPointSheet)
             {
                 if (point.TerritoryType.Row <= 1)
                     continue;
@@ -82,9 +69,10 @@ public class ExtendedGatheringItem : GatheringItem
             }
 
             return _gatheringPoints ??= pointBases
-                .Select((baseId) => GatheringPointSheet.Where((row) => row.TerritoryType.Row > 1 && row.GatheringPointBase.Row == baseId))
+                .Select((baseId) => gatheringPointSheet.Where((row) => row.TerritoryType.Row > 1 && row.GatheringPointBase.Row == baseId))
                 .SelectMany(e => e)
-                .OfType<ExtendedGatheringPoint>().ToArray();
+                .OfType<ExtendedGatheringPoint>()
+                .ToArray();
         }
     }
 }
