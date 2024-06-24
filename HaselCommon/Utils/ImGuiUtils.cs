058ae0a8ff542bb9c5ab41f467a82d255764b812
Haselnussbomber <mail@haselnussbomber.de>
2024-06-24 20:49:35 +0200

    add ImGuiUtils EndUnconditionally and EndConditionally

--- HaselCommon/Utils/ImGuiUtils.cs
+++ HaselCommon/Utils/ImGuiUtils.cs
@@ -181,4 +181,44 @@ public static class ImGuiUtils
 
         return pressed;
     }
+
+    public struct EndUnconditionally(Action endAction, bool success) : ImRaii.IEndObject, IDisposable
+    {
+        private Action EndAction { get; } = endAction;
+
+        public bool Success { get; } = success;
+
+        public bool Disposed { get; private set; } = false;
+
+        public void Dispose()
+        {
+            if (!Disposed)
+            {
+                EndAction();
+                Disposed = true;
+            }
+        }
+    }
+
+    public struct EndConditionally(Action endAction, bool success) : ImRaii.IEndObject, IDisposable
+    {
+        public bool Success { get; } = success;
+
+        public bool Disposed { get; private set; } = false;
+
+        private Action EndAction { get; } = endAction;
+
+        public void Dispose()
+        {
+            if (!Disposed)
+            {
+                if (Success)
+                {
+                    EndAction();
+                }
+
+                Disposed = true;
+            }
+        }
+    }
 }
