95ce9ebfcea9fa941e7876b977cfadcdae7045a0
Haselnussbomber <mail@haselnussbomber.de>
2025-02-15 20:10:54 +0100

    UnlocksObserver deadlock workaround

--- HaselCommon/Services/UnlocksObserver.cs
+++ HaselCommon/Services/UnlocksObserver.cs
@@ -1,7 +1,7 @@
 using System.Linq;
 using Dalamud.Hooking;
 using Dalamud.Plugin.Services;
-using FFXIVClientStructs.FFXIV.Client.UI;
+using FFXIVClientStructs.FFXIV.Client.UI.Misc;
 using Microsoft.Extensions.Logging;
 
 namespace HaselCommon.Services;
@@ -9,8 +9,10 @@ namespace HaselCommon.Services;
 [RegisterSingleton]
 public unsafe class UnlocksObserver : IDisposable
 {
+    private delegate void RaptureHotbarModulePerformMateriaActionMigrationDelegate(RaptureHotbarModule* thisPtr);
+
     private readonly ILogger<UnlocksObserver> _logger;
-    private readonly Hook<RaptureAtkModule.Delegates.Update> _updateHook;
+    private readonly Hook<RaptureHotbarModulePerformMateriaActionMigrationDelegate> _hook;
 
     public event Action? Update;
 
@@ -18,22 +20,28 @@ public unsafe class UnlocksObserver : IDisposable
     {
         _logger = logger;
 
-        _updateHook = gameInteropProvider.HookFromAddress<RaptureAtkModule.Delegates.Update>(
-            RaptureAtkModule.StaticVirtualTablePointer->Update,
-            RaptureAtkModuleUpdateDetour);
+        // This might look weird at first, but this function is called inside RaptureAtkModule.Update
+        // when RaptureAtkModule.AgentUpdateFlag has flag UnlocksUpdate, which is exactly what I need.
+        // Previously, this class hooked RaptureAtkModule.Update directly, but this caused a deadlock
+        // when enabling the hook for some reason. See #dalamud-dev:
+        // https://discord.com/channels/581875019861328007/860813266468732938/1322031324390756373
 
-        _updateHook.Enable();
+        _hook = gameInteropProvider.HookFromSignature<RaptureHotbarModulePerformMateriaActionMigrationDelegate>(
+            "E8 ?? ?? ?? ?? 48 8D 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 8B 8B ?? ?? ?? ?? 48 8B 01",
+            RaptureHotbarModulePerformMateriaActionMigrationDetour);
+
+        _hook.Enable();
     }
 
     public void Dispose()
     {
-        _updateHook.Dispose();
+        _hook.Dispose();
         GC.SuppressFinalize(this);
     }
 
-    private void RaptureAtkModuleUpdateDetour(RaptureAtkModule* module, float delta)
+    private void RaptureHotbarModulePerformMateriaActionMigrationDetour(RaptureHotbarModule* thisPtr)
     {
-        if (Update != null && module->AgentUpdateFlag.HasFlag(RaptureAtkModule.AgentUpdateFlags.UnlocksUpdate))
+        if (Update != null)
         {
             foreach (var action in Update.GetInvocationList().Cast<Action>())
             {
@@ -48,6 +56,6 @@ public unsafe class UnlocksObserver : IDisposable
             }
         }
 
-        _updateHook.OriginalDisposeSafe(module, delta);
+        _hook.OriginalDisposeSafe(thisPtr);
     }
 }
