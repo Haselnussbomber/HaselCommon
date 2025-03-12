cc92017ad3575d2e4da4a426d434d6d2729bb02d
Haselnussbomber <mail@haselnussbomber.de>
2025-03-12 16:28:32 +0100

    Use Telepo directly in TeleportService

--- HaselCommon/Services/TeleportService.cs
+++ HaselCommon/Services/TeleportService.cs
@@ -1,39 +1,22 @@
-using System.Collections.Frozen;
-using System.Collections.Generic;
-using System.Linq;
 using System.Numerics;
 using Dalamud.Plugin.Services;
+using FFXIVClientStructs.FFXIV.Client.Game.Control;
+using FFXIVClientStructs.FFXIV.Client.Game.UI;
 using HaselCommon.Game.Enums;
-using Lumina.Excel;
 using Lumina.Excel.Sheets;
 
 namespace HaselCommon.Services;
 
-[RegisterSingleton]
-public class TeleportService : IDisposable
+[RegisterSingleton, AutoConstruct]
+public unsafe partial class TeleportService : IDisposable
 {
-    private readonly FrozenDictionary<uint, MapMarker> _aetheryteMapMarkers;
-    private readonly IAetheryteList _aetheryteList;
     private readonly IClientState _clientState;
     private readonly ExcelService _excelService;
     private readonly UnlocksObserver _unlocksObserver;
-    private readonly List<(Aetheryte Aetheryte, MapMarker Marker)> _aetherytes = [];
 
-    public TeleportService(IAetheryteList aetheryteList, IClientState clientState, ExcelService excelService, UnlocksObserver unlocksObserver)
+    [AutoPostConstruct]
+    private void Initialize()
     {
-        _aetheryteList = aetheryteList;
-        _clientState = clientState;
-        _excelService = excelService;
-        _unlocksObserver = unlocksObserver;
-
-        _aetheryteMapMarkers = excelService.GetSubrowSheet<MapMarker>()
-            .SelectMany(subrows => subrows)
-            .Cast<MapMarker?>()
-            .Where(mapMarker => mapMarker != null && (MapMarkerDataType)mapMarker.Value.DataType == MapMarkerDataType.Aetheryte)
-            .Cast<MapMarker>()
-            .DistinctBy(m => m.DataKey.RowId)
-            .ToFrozenDictionary(m => m.DataKey.RowId, m => m);
-
         _clientState.Login += UpdateAetherytes;
         _unlocksObserver.Update += UpdateAetherytes;
 
@@ -49,57 +32,54 @@ public class TeleportService : IDisposable
 
     private void UpdateAetherytes()
     {
-        _aetherytes.Clear();
-
-        foreach (var entry in _aetheryteList)
-        {
-            if (!entry.AetheryteData.IsValid || !_aetheryteMapMarkers.TryGetValue(entry.AetheryteData.Value.RowId, out var marker))
-                continue;
-
-            _aetherytes.Add((entry.AetheryteData.Value, marker));
-        }
+        if (Control.GetLocalPlayer() != null)
+            Telepo.Instance()->UpdateAetheryteList();
     }
 
     public uint GetTeleportCost(uint destinationTerritoryTypeId)
     {
         var cost = 0u;
 
-        foreach (var aetheryte in _aetheryteList)
+        foreach (var teleportInfo in Telepo.Instance()->TeleportList)
         {
-            if (aetheryte.TerritoryId == destinationTerritoryTypeId && (cost == 0 || aetheryte.GilCost < cost))
+            if (teleportInfo.TerritoryId == destinationTerritoryTypeId && (cost == 0 || teleportInfo.GilCost < cost))
             {
-                cost = aetheryte.GilCost;
+                cost = teleportInfo.GilCost;
             }
         }
 
         return cost;
     }
 
-    public bool TryGetClosestAetheryte(Level level, out RowRef<Aetheryte> aetheryte)
+    public bool TryGetClosestAetheryte(Level level, out Aetheryte aetheryte)
     {
-        var levelCoords = new Vector2(level.X, level.Z);
-
-        var aetheryteId = level.RowId switch
+        // Ehll Tou or Charlemend are in the Firmament, so use Foundation instead
+        if (level.RowId is 8370121 or 8658159)
         {
-            // Ehll Tou or Charlemend are in the Firmament, so use Foundation instead
-            8370121 or 8658159 => 70u,
+            return _excelService.TryGetRow(70, out aetheryte);
+        }
 
-            // Find Aetheryte in the same Map and TerritoryType
-            _ => _aetherytes
-                .Where(entry => entry.Aetheryte.Map.RowId == level.Map.RowId && entry.Aetheryte.Territory.RowId == level.Territory.RowId)
-                .Select(entry => new
+        var levelCoords = new Vector2(level.X, level.Z);
+        var aetherytes = _excelService.FindRows<Aetheryte>(row => row.Map.RowId == level.Map.RowId && row.Territory.RowId == level.Territory.RowId);
+
+        aetheryte = default;
+        var currentDistance = float.MaxValue;
+
+        foreach (var aetheryteRow in aetherytes)
+        {
+            if (_excelService.TryFindSubrow<MapMarker>(row => row.DataType == (byte)MapMarkerDataType.Aetheryte && row.DataKey.RowId == aetheryteRow.RowId, out var mapMarker))
+            {
+                var distance = (MapService.GetCoords(aetheryteRow.Map.Value, mapMarker) - levelCoords).LengthSquared();
+                if (distance < currentDistance)
                 {
-                    entry.Aetheryte,
-                    LengthSquared = (MapService.GetCoords(entry.Aetheryte.Map.Value, entry.Marker) - levelCoords).LengthSquared()
-                })
-                .OrderBy(entry => entry.LengthSquared)
-                .FirstOrDefault()?
-                .Aetheryte.RowId ?? 0u
-        };
+                    currentDistance = distance;
+                    aetheryte = aetheryteRow;
+                }
+            }
+        }
 
         // TODO: if no aetheryte was found in the same territory, find closest one in adjacent territories
 
-        aetheryte = _excelService.CreateRef<Aetheryte>(aetheryteId);
-        return aetheryteId != 0;
+        return currentDistance != float.MaxValue;
     }
 }
