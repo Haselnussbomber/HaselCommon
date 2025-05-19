bb691c8fd4ed196399157b6441c63d7dcfc18e33
Haselnussbomber <mail@haselnussbomber.de>
2025-05-19 02:03:51 +0200

    Remove old code

--- HaselCommon/Game/Misc.cs
+++ HaselCommon/Game/Misc.cs
@@ -4,10 +4,6 @@ namespace HaselCommon.Game;
 
 public static class Misc
 {
-    // "80 F9 07 77 0D"
-    public static bool IsGatheringTypeRare(byte id)
-        => id > 7 || (0x8B & 1 << id) == 0;
-
     public static unsafe bool IsLightTheme
         => RaptureAtkModule.Instance()->AtkUIColorHolder.ActiveColorThemeType == 1;
 }
