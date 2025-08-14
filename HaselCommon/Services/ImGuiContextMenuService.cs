bc1b4c92666cfbada0e0f3a5821439257ef8fcaa
Haselnussbomber <mail@haselnussbomber.de>
2025-08-14 02:02:32 +0200

    Use ClearWindowFocus

--- HaselCommon/Services/ImGuiContextMenuService.cs
+++ HaselCommon/Services/ImGuiContextMenuService.cs
@@ -348,7 +348,7 @@ public struct ImGuiContextMenuEntry : IImGuiContextMenuEntry
             ClickCallback?.Invoke();
 
             if (LoseFocusOnClick)
-                ImGui.SetWindowFocus();
+                ImGui.ClearWindowFocus();
         }
         if (ImGui.IsItemHovered())
             HoverCallback?.Invoke();
