2c69f0db8a7f98ad6b673a98666737fe360ce0ef
Haselnussbomber <mail@haselnussbomber.de>
2025-09-06 14:38:00 +0200

    Credit where credit is due

--- HaselCommon/Utils/Internal/DalamudServiceWrapper.cs
+++ HaselCommon/Utils/Internal/DalamudServiceWrapper.cs
@@ -2,6 +2,7 @@ using Dalamud.IoC;
 
 namespace HaselCommon.Utils.Internal;
 
+//! https://github.com/Ottermandias/OtterGui/blob/3bf047b/Services/ServiceManager.cs#L133
 internal class DalamudServiceWrapper<T>
 {
     [PluginService] internal T Service { get; private set; } = default!;
