6cf2ae506c7026938915959cddfa1d1273f3af13
Haselnussbomber <mail@haselnussbomber.de>
2025-10-22 08:42:43 +0200

    Add StringExtensions.Until/After

--- HaselCommon/Extensions/Strings/StringExtensions.cs
+++ HaselCommon/Extensions/Strings/StringExtensions.cs
@@ -4,6 +4,24 @@ namespace HaselCommon.Extensions;
 
 public static class StringExtensions
 {
+    public static string Until(this string input, string delimiter)
+    {
+        if (string.IsNullOrEmpty(input) || string.IsNullOrEmpty(delimiter))
+            return input;
+
+        var idx = input.IndexOf(delimiter, StringComparison.Ordinal);
+        return idx >= 0 ? input[..idx] : input;
+    }
+
+    public static string After(this string input, string delimiter)
+    {
+        if (string.IsNullOrEmpty(input) || string.IsNullOrEmpty(delimiter))
+            return input;
+
+        var idx = input.IndexOf(delimiter, StringComparison.Ordinal);
+        return idx >= 0 ? input[(idx + delimiter.Length)..] : input;
+    }
+
     public static string ToKebabCase(this string input)
     {
         if (string.IsNullOrEmpty(input))
