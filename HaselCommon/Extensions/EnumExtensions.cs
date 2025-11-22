e593cfa3c0adda63aa9d72c5d81bb80dff2bba89
Haselnussbomber <mail@haselnussbomber.de>
2025-11-22 23:31:35 +0100

    Add EnumExtensions

---
+++ HaselCommon/Extensions/EnumExtensions.cs
@@ -0,0 +1,29 @@
+using System.Runtime.CompilerServices;
+
+namespace HaselCommon.Extensions;
+
+public static class EnumExtensions
+{
+    public static unsafe void SetFlag<T>(this ref T flags, T flag, bool enable = true) where T : unmanaged, Enum
+    {
+        switch (sizeof(T))
+        {
+            case 1: flags.SetFlag<T, byte>(flag, enable); break;
+            case 2: flags.SetFlag<T, ushort>(flag, enable); break;
+            case 4: flags.SetFlag<T, uint>(flag, enable); break;
+            case 8: flags.SetFlag<T, ulong>(flag, enable); break;
+            default: throw new NotSupportedException("Unsupported enum size");
+        }
+    }
+
+    public static void SetFlag<TEnum, TUnderlying>(this ref TEnum flags, TEnum flag, bool enable = true)
+        where TEnum : unmanaged, Enum
+        where TUnderlying : unmanaged, IBinaryInteger<TUnderlying>
+    {
+        ref var value = ref Unsafe.As<TEnum, TUnderlying>(ref flags);
+        var bit = Unsafe.As<TEnum, TUnderlying>(ref flag);
+        var current = (value & bit) != TUnderlying.Zero;
+        if (current != enable)
+            value ^= bit;
+    }
+}
