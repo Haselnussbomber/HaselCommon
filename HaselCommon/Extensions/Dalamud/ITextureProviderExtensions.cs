6189957741eb72e6a42f7c3f573b6cf82f6e149e
Haselnussbomber <mail@haselnussbomber.de>
2025-11-28 14:50:47 +0100

    Add ITextureProviderExtensions.TryGetIconSize

--- HaselCommon/Extensions/Dalamud/ITextureProviderExtensions.cs
+++ HaselCommon/Extensions/Dalamud/ITextureProviderExtensions.cs
@@ -1,9 +1,39 @@
 using Dalamud.Interface.Textures;
+using HaselCommon.Extensions.Dalamud;
+using Lumina.Data.Files;
 
 namespace HaselCommon.Extensions;
 
 public static class ITextureProviderExtensions
 {
+    private static readonly Dictionary<uint, Vector2> IconSizeCache = [];
+
+    public static bool TryGetIconSize(this ITextureProvider textureProvider, uint iconId, out Vector2 size)
+    {
+        if (IconSizeCache.TryGetValue(iconId, out size))
+            return size != Vector2.Zero;
+
+        var iconPath = textureProvider.GetIconPath(iconId);
+        if (string.IsNullOrEmpty(iconPath))
+        {
+            size = Vector2.Zero;
+            IconSizeCache[iconId] = size;
+            return false;
+        }
+
+        if (!ServiceLocator.TryGetService<IDataManager>(out var dataManager)
+            || !dataManager.TryGetFile<TexFile>(iconPath, out var file))
+        {
+            size = Vector2.Zero;
+            IconSizeCache[iconId] = size;
+            return false;
+        }
+
+        size = new Vector2(file.Header.Width, file.Header.Height);
+        IconSizeCache[iconId] = size;
+        return size != Vector2.Zero;
+    }
+
     public static void Draw(this ITextureProvider textureProvider, string path, DrawInfo drawInfo)
     {
         if (drawInfo.IsRectVisible
