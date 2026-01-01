af3b7be453345963050fc3ac519e610d8df111c5
Haselnussbomber <mail@haselnussbomber.de>
2026-01-01 05:24:55 +0100

    Add AtkUnitBaseExtensions.(Scaled)Size

--- HaselCommon/Extensions/Game/AtkUnitBaseExtensions.cs
+++ HaselCommon/Extensions/Game/AtkUnitBaseExtensions.cs
@@ -2,10 +2,32 @@ using FFXIVClientStructs.FFXIV.Component.GUI;
 
 namespace HaselCommon.Extensions;
 
-public static class AtkUnitBaseExtensions
+public static unsafe class AtkUnitBaseExtensions
 {
-    extension(AtkUnitBase addon)
+    extension(AtkUnitBase unitBase)
     {
-        public Vector2 Position => new(addon.X, addon.Y);
+        public Vector2 Position => new(unitBase.X, unitBase.Y);
+
+        public Vector2 Size
+        {
+            get
+            {
+                short width;
+                short height;
+                unitBase.GetSize(&width, &height, false);
+                return new Vector2(width, height);
+            }
+        }
+
+        public Vector2 ScaledSize
+        {
+            get
+            {
+                short width;
+                short height;
+                unitBase.GetSize(&width, &height, true);
+                return new Vector2(width, height);
+            }
+        }
     }
 }
