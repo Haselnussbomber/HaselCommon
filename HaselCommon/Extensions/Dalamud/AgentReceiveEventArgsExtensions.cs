020eebec82990598ab62cf6012eebe9450069a9e
Haselnussbomber <mail@haselnussbomber.de>
2026-01-11 17:50:32 +0100

    Add AgentReceiveEventArgsExtensions

---
+++ HaselCommon/Extensions/Dalamud/AgentReceiveEventArgsExtensions.cs
@@ -0,0 +1,12 @@
+using Dalamud.Game.Agent.AgentArgTypes;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace HaselCommon.Extensions;
+
+public static unsafe class AgentReceiveEventArgsExtensions
+{
+    extension(AgentReceiveEventArgs args)
+    {
+        public Span<AtkValue> GetAtkValues() => new((void*)args.AtkValues, (int)args.ValueCount);
+    }
+}
