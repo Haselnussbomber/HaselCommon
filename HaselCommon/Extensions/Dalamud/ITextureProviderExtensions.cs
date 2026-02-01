cd3b8a390f86d273c97c100a41a0972abb4130f2
Haselnussbomber <mail@haselnussbomber.de>
2026-02-01 11:35:41 +0100

    Remove TryGetIconSize

--- HaselCommon/Extensions/Dalamud/ITextureProviderExtensions.cs
+++ HaselCommon/Extensions/Dalamud/ITextureProviderExtensions.cs
@@ -1,40 +1,11 @@
 using Dalamud.Interface.Textures;
-using Lumina.Data.Files;
 
 namespace HaselCommon.Extensions;
 
 public static class ITextureProviderExtensions
 {
-    private static readonly Dictionary<uint, Vector2> IconSizeCache = [];
-
     extension(ITextureProvider textureProvider)
     {
-        public bool TryGetIconSize(uint iconId, out Vector2 size)
-        {
-            if (IconSizeCache.TryGetValue(iconId, out size))
-                return size != Vector2.Zero;
-
-            var iconPath = textureProvider.GetIconPath(iconId);
-            if (string.IsNullOrEmpty(iconPath))
-            {
-                size = Vector2.Zero;
-                IconSizeCache[iconId] = size;
-                return false;
-            }
-
-            if (!ServiceLocator.TryGetService<IDataManager>(out var dataManager)
-                || !dataManager.TryGetFile<TexFile>(iconPath, out var file))
-            {
-                size = Vector2.Zero;
-                IconSizeCache[iconId] = size;
-                return false;
-            }
-
-            size = new Vector2(file.Header.Width, file.Header.Height);
-            IconSizeCache[iconId] = size;
-            return size != Vector2.Zero;
-        }
-
         public void Draw(string path, DrawInfo drawInfo)
         {
             if (drawInfo.IsRectVisible
