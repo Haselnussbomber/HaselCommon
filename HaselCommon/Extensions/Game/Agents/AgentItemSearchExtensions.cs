e03a54559bc2ad7fdefea2a8389200018b0fdf0c
Haselnussbomber <mail@haselnussbomber.de>
2026-02-01 11:38:58 +0100

    Cleanup dependency on ClientState in SearchForItem

--- HaselCommon/Extensions/Game/Agents/AgentItemSearchExtensions.cs
+++ HaselCommon/Extensions/Game/Agents/AgentItemSearchExtensions.cs
@@ -1,3 +1,4 @@
+using FFXIVClientStructs.FFXIV.Client.System.Framework;
 using FFXIVClientStructs.FFXIV.Client.UI;
 using FFXIVClientStructs.FFXIV.Client.UI.Agent;
 using FFXIVClientStructs.FFXIV.Component.GUI;
@@ -23,16 +24,14 @@ public static unsafe class AgentItemSearchExtensions
             if (!agent.CanSearchForItem(item))
                 return;
 
-            if (!ServiceLocator.TryGetService<IClientState>(out var clientState))
-                return;
-
             if (!TryGetAddon<AddonItemSearch>((ushort)agent.GetAddonId(), out var addon))
                 return;
 
             if (TryGetAddon<AtkUnitBase>("ItemSearchResult"u8, out var itemSearchResult))
                 itemSearchResult->Hide2();
 
-            var itemName = item.GetItemName(clientState.ClientLanguage).ToString();
+            var clientLanguage = (ClientLanguage)Framework.Instance()->ClientLanguage;
+            var itemName = item.GetItemName(clientLanguage).ToString();
             if (itemName.Length > 40)
                 itemName = itemName[..40];
 
