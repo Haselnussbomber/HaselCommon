04bd60315033ff77a46aefdd491697c43924b967
Haselnussbomber <mail@haselnussbomber.de>
2026-03-13 14:46:16 +0100

    Add AddonArgs and AddonReceiveEventArgs extensions

--- HaselCommon/Extensions/Dalamud/IAddonLifecycleExtensions.cs
+++ HaselCommon/Extensions/Dalamud/IAddonLifecycleExtensions.cs
@@ -5,6 +5,12 @@ namespace HaselCommon.Extensions;
 
 public static unsafe class IAddonLifecycleExtensions
 {
+    
+    extension(AddonArgs args)
+    {
+        public T* GetAddon<T>() where T : unmanaged => (T*)args.Addon.Address;
+    }
+
     extension(AddonRefreshArgs args)
     {
         public Span<AtkValue> GetAtkValues() => new((void*)args.AtkValues, (int)args.AtkValueCount);
@@ -13,5 +19,11 @@ public static unsafe class IAddonLifecycleExtensions
     extension(AddonSetupArgs args)
     {
         public Span<AtkValue> GetAtkValues() => new((void*)args.AtkValues, (int)args.AtkValueCount);
+    }
+
+    extension(AddonReceiveEventArgs args)
+    {
+        public AtkEventType EventType => (AtkEventType)args.AtkEventType;
+        public T* GetEventData<T>() where T : unmanaged => (T*)args.AtkEventData;
     }
 }
