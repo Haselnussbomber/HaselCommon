81b0075f4db4cc35b265813f4d1b3140fdc6242b
Haselnussbomber <mail@haselnussbomber.de>
2026-09-05 20:14:37 +0200

    Remove RowRefExtensions

--- HaselCommon/Services/ItemService.cs
+++ HaselCommon/Services/ItemService.cs
@@ -364,7 +364,7 @@ public partial class ItemService
         if (race == 0)
             return false;
 
-        if (!itemRow.EquipRestriction.TryGetRow(out var equipRaceCategoryRow))
+        if (!itemRow.EquipRestriction.TryGetValue(out var equipRaceCategoryRow))
             return false;
 
         if (!equipRaceCategoryRow.Races[(int)race - 1])
@@ -421,7 +421,7 @@ public partial class ItemService
             return false;
         }
 
-        if (!TryGetItem(item, out var itemRow) || !itemRow.EquipRestriction.TryGetRow(out var equipRaceCategoryRow))
+        if (!TryGetItem(item, out var itemRow) || !itemRow.EquipRestriction.TryGetValue(out var equipRaceCategoryRow))
         {
             errorLogMessage = 716; // "Unable to move item. Please try again. (Reading data...)" ????
             entry.CanEquipCache[key] = (false, errorLogMessage);
