3dbdc29d0164c936b06e0c7ae3beea9ec3de2eb5
Haselnussbomber <mail@haselnussbomber.de>
2026-09-05 20:24:49 +0200

    Move IGameInteropProviderExtensions functions

--- HaselCommon/Extensions/Dalamud/IGameInteropProviderExtensions.cs
+++ HaselCommon/Extensions/Dalamud/IGameInteropProviderExtensions.cs
@@ -1,17 +1,38 @@
 namespace HaselCommon.Extensions;
 
-public static partial class IGameInteropProviderExtensions
+public static class IGameInteropProviderExtensions
 {
     extension(IGameInteropProvider gameInteropProvider)
     {
-        public unsafe Hook<T> HookFromVTable<T>(void* vtblAddress, int vfIndex, T detour) where T : Delegate
+        public Hook<TDelegate> EnabledHookFromAddress<TDelegate>(nint address, TDelegate detour) where TDelegate : Delegate
         {
-            return gameInteropProvider.HookFromVTable((nint)vtblAddress, vfIndex, detour);
+            var hook = gameInteropProvider.HookFromAddress(address, detour);
+            hook.Enable();
+            return hook;
         }
 
-        public unsafe Hook<T> HookFromVTable<T>(nint vtblAddress, int vfIndex, T detour) where T : Delegate
+        public unsafe Hook<TDelegate> EnabledHookFromAddress<TDelegate>(void* address, TDelegate detour) where TDelegate : Delegate
         {
-            return gameInteropProvider.HookFromAddress(*(nint*)(vtblAddress + vfIndex * 0x08), detour);
+            var hook = gameInteropProvider.HookFromAddress((nint)address, detour);
+            hook.Enable();
+            return hook;
+        }
+
+        public Hook<TDelegate> EnabledHookFromSignature<TDelegate>(string signature, TDelegate detour) where TDelegate : Delegate
+        {
+            var hook = gameInteropProvider.HookFromSignature(signature, detour);
+            hook.Enable();
+            return hook;
+        }
+
+        public unsafe Hook<TDelegate> EnabledHookFromVTable<TDelegate>(void* vtblAddress, int vfIndex, TDelegate detour) where TDelegate : Delegate
+        {
+            return gameInteropProvider.EnabledHookFromVTable((nint)vtblAddress, vfIndex, detour);
+        }
+
+        public unsafe Hook<TDelegate> EnabledHookFromVTable<TDelegate>(nint vtblAddress, int vfIndex, TDelegate detour) where TDelegate : Delegate
+        {
+            return gameInteropProvider.EnabledHookFromAddress(*(nint*)(vtblAddress + vfIndex * 0x08), detour);
         }
     }
 }
